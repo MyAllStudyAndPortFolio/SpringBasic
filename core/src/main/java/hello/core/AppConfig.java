2c6f04e73b1789379588c5786ba09af0579e0050
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2023-01-07 10:22:18 +0900

    feat : change to spring

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -9,9 +9,16 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 //AppConfig 는 생성한 객체 인스턴스의 참조를 "생성자를 통해서 주입" 해준다.
+
+@Configuration
 public class AppConfig {
     //AppConfig Refactoring
+    // 스프링 Bean은 @Bean 이 붙은 메서드 명을 스프링 빈의 이름으로 사용
+
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
@@ -20,15 +27,17 @@ public class AppConfig {
      *  MemoryMemberRepository, FixDiscountPolicy 를
      *  밖으로 빼냄
      *  */
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+    @Bean
     public DiscountPolicy discountPolicy(){
         // fix 에서 Rate 로 교체한다. 구성영역 변경
         //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(
                 memberRepository(),discountPolicy());
--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -4,15 +4,20 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+ //       AppConfig appConfig = new AppConfig();
         // 이제는 AppConfig에서 다 결정
-        MemberService memberService = appConfig.memberService();
+  //      MemberService memberService = appConfig.memberService();
         // 원래는 Impl에서 직접 생성
- //       MemberService memberService = new MemberServiceImpl();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        //       MemberService memberService = new MemberServiceImpl();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
